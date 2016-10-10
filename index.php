1a93e887400d96c9a7fa6891d8964ea61810b962
igor okhotnikov <indexseo@mail.ru>
2016-10-10 23:30:33 +0500

    form-1

---
+++ index.php
@@ -0,0 +1,9 @@
+<body>
+<form action="/form" method="POST">
+<input type="TEXT" name="fio">
+<input type="TEXT" name="site">
+<input type="TEXT" name="mail">
+<input type="TEXT" name="tel">
+<input type="TEXT" name="txt">
+<input type="submit" value="Узнать">
+</form>
